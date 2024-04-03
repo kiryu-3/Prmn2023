104f1d2d2a09e46fd4bba73a05a1ea7ab6a40a99
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-04-03 15:07:47 +0900

    Create app.py

---
+++ Python/LINEBot/app.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+import uuid
+
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage
+import dotenv
+from pyngrok import ngrok
+
+app = Flask(__name__)
+# LineBotApiオブジェクトを作成
+dotenv.load_dotenv("content/info/.env")
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)  # リクエストの署名検証を行い、正しければハンドラを実行
+    except InvalidSignatureError:
+        abort(400)  # 署名が無効な場合はエラーを返す
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    reply_text = event.message.text  # 受信したテキストメッセージを取得
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_text)  # 受信したテキストメッセージをそのまま返信
+    )
+    print("返信完了!!\ntext:", event.message.text)  # 返信が完了したことを表示
+
+ngrok_tunnel = ngrok.connect(5000)  # ポート5000でngrokのトンネルを作成
+print('Public URL:', ngrok_tunnel.public_url)  # 公開されたURLを表示
+
+if __name__ == "__main__":
+    app.run()  # アプリケーションを実行
